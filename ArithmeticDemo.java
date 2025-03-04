cf6f33c7c521ca180bc41ecaa1c8ffa055f13b9f
Jhecicatadlip <tadlipjhecica_bscs@plmun.edu.ph>
2025-03-04 13:30:30 +0800

    "Submitting OBJPROG Week 02 - Session 01 - Exercise 03"

--- ArithmeticDemo.java
+++ ArithmeticDemo.java
@@ -1,8 +1,5 @@
 public class ArithmeticDemo {
     public static void main(String[] args) {
-
-    }
-}
 int num1 = 10;
 int num2 = 5;
 double num3 = 3.0;
@@ -18,3 +15,5 @@ System.out.println("Product: " + product);
 System.out.println("Quotient: " + quotient);
 System.out.println("Remainder: " + remainder);
 System.out.println("Expression Result: " + expressionResult); // Explain order of operations
+    }
+}
